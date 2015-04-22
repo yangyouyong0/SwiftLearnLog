7c2d2f9cacd5d70858c5e684a8e3de7fb4cb4cb3
yangyouyong <yangyouyong@hujiang.com>
2015-04-22 18:32:43 +0800

    [Feature]属性的学习，类和结构体

--- MySwift03.playground/Contents.swift
+++ MySwift03.playground/Contents.swift
@@ -23,3 +23,16 @@ func changeIO(inout str1:String , inout str2:String) {
 
 changeIO(&str, &str1)
 println(str)
+
+//  解释: swift 中集合（Collection）类型均以结构体的形式实现，String，Array，Dictionary，Set 类型的数据被赋值是发生值拷贝行为
+//  Objective－C 中 集合类型均以类的形式实现， 在赋值和传入函数时，不会发生值拷贝，传递的是实例的引用
+ println("\(str)")
+
+//  常量 存储属性
+//  当值类型的实例被声明为常量时， 它所有的属性也成为了常量
+//  引用类型的类不一样，把引用类型的实例赋给常量后，仍然可以修改其变量的属性
+
+
+
+
+
