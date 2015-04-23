8917e4644f7b7f00bbfcef52d770e96c49099530
yangyouyong <yangyouyong@hujiang.com>
2015-04-23 18:42:00 +0800

    属性，方法

--- MySwift04.playground/Contents.swift
+++ MySwift04.playground/Contents.swift
@@ -3,3 +3,22 @@
 import UIKit
 
 var str = "Hello, playground"
+//  -------------------------方法-------------------------  //
+
+//  结构体和枚举能够定义方法是Swift与OC的主要区别之一
+
+//  final 关键字会防止方法重写 类前面加final 类会不让继承
+
+final class finalClass {
+   final var name = ""
+}
+//class subClass: finalClass {
+//    println("")
+//}
+
+//  -------------------------构造过程-------------------------  //
+
+
+
+
+
